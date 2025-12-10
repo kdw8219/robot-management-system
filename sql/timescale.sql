31836ffb9a88f7db2f18b56cf5fc793fc02fffa6
kdw8219 <kdw8219@gmail.com>
2025-12-10 15:28:54 +0900

    Feature  - add robot status, pos table

--- sql/timescale.sql
+++ sql/timescale.sql
@@ -3,7 +3,6 @@ CREATE EXTENSION IF NOT EXISTS timescaledb;
 CREATE TABLE robot_heartbeat (
     robot_id     TEXT NOT NULL,
     is_alive     BOOLEAN NOT NULL,
-    stream_ip     TEXT NOT NULL,
     timestamp           TIMESTAMPTZ NOT NULL,
     PRIMARY KEY (robot_id, timestamp)
 );
@@ -18,3 +17,50 @@ ALTER TABLE robot_heartbeat SET (
 );
 
 SELECT add_compression_policy('robot_heartbeat', INTERVAL '7 days');
+
+
+CREATE TABLE robot_status (
+    robot_id     TEXT NOT NULL,
+    battery     DOUBLE PRECISION NOT NULL,
+    status     TEXT NOT NULL,
+    error     TEXT NOT NULL,
+    timestamp           TIMESTAMPTZ NOT NULL,
+    PRIMARY KEY (robot_id, timestamp)
+);
+
+SELECT create_hypertable('robot_status', 'timestamp', if_not_exists => TRUE);
+
+SELECT add_retention_policy('robot_status', INTERVAL '10 days');
+
+ALTER TABLE robot_status SET (
+    timescaledb.compress,
+    timescaledb.compress_segmentby = 'robot_id'
+);
+
+SELECT add_compression_policy('robot_status', INTERVAL '7 days');
+
+CREATE TABLE robot_pos (
+    robot_id     TEXT NOT NULL,
+    x           DOUBLE PRECISION NOT NULL,
+    y           DOUBLE PRECISION NOT NULL,
+    z           DOUBLE PRECISION NOT NULL,
+    orig_x           DOUBLE PRECISION NOT NULL,
+    orig_y           DOUBLE PRECISION NOT NULL,
+    orig_z           DOUBLE PRECISION NOT NULL,
+    orig_w           DOUBLE PRECISION NOT NULL,
+    linear_angle           DOUBLE PRECISION NOT NULL,
+    angular_speed           DOUBLE PRECISION NOT NULL,
+    timestamp           TIMESTAMPTZ NOT NULL,
+    PRIMARY KEY (robot_id, timestamp)
+);
+
+SELECT create_hypertable('robot_pos', 'timestamp', if_not_exists => TRUE);
+
+SELECT add_retention_policy('robot_pos', INTERVAL '10 days');
+
+ALTER TABLE robot_pos SET (
+    timescaledb.compress,
+    timescaledb.compress_segmentby = 'robot_id'
+);
+
+SELECT add_compression_policy('robot_pos', INTERVAL '7 days');
